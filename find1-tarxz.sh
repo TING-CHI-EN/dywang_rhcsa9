10f14c6a04388abf7c8c56012463a7c05123a924
TING-CHI-EN <121035540+TING-CHI-EN@users.noreply.github.com>
2023-12-08 00:47:02 +0800

    Update find1-tarxz.sh

--- find1-tarxz.sh
+++ find1-tarxz.sh
@@ -32,13 +32,58 @@ cd ..
 cd mytar.xz/
 tar Jxvf /root/tarff.tar.xz
 
-
-# 都還不對
 ssh deyu5@kvm8
+
+podman pull registry.csie.cyut.edu.tw/rsyslog
+podman pull registry.csie.cyut.edu.tw/dywrsyslog
+
 echo 'FROM registry.csie.cyut.edu.tw/rsyslog
 MAINTAINER dywang@csie.cyut.edu.tw
 
 RUN echo 'local4.*     /var/log/journal/podmanfile.log' >> /etc/rsyslog.conf' > Podmanfile
+
+podman build --tag=pmrsyslog --file Podmanfile
+podman build --tag=podimg http://dywang.csie.cyut.edu.tw/materials/Podmanfile
+
+podman create --name mserver dywrsyslog
+podman create --name pserver podimg
+
+mkdir -p .config/systemd/user/
+cd .config/systemd/user/
+
+podman generate systemd --name mserver --files 
+podman generate systemd --name pserver --files
+
+systemctl --user daemon-reload 
+systemctl --user enable --now container-mserver.service 
+systemctl --user enable --now container-pserver.service
+
+exit
+
+loginctl enable-linger deyu5
+
+reboot
+
+ssh deyu5@kvm8
+
+mkdir cjournal1
+mkdir djournal1
+
+podman create --name mserver --privileged --volume /home/deyu5/cjournal1/:/var/log/journal/:Z dywrsyslog:latest
+podman create --name pserver --privileged --volume /home/deyu5/djournal1/:/var/log/journal/:Z podimg:latest
+
+mkdir -p .config/systemd/user/
+cd .config/systemd/user/
+
+podman generate systemd --name mserver --files
+podman generate systemd --name pserver --files
+systemctl --user daemon-reload
+
+
+
+# 都還不對
+ssh deyu5@kvm8
+
 
 podman pull registry.csie.cyut.edu.tw/dywrsyslog
 podman build --tag podimg --file Podmanfile
