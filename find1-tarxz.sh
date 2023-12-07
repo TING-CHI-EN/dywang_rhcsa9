6191a3ad531ed6588175faeba1efd2d313b8ecad
TING-CHI-EN <121035540+TING-CHI-EN@users.noreply.github.com>
2023-12-07 10:00:33 +0800

    Update find1-tarxz.sh

--- find1-tarxz.sh
+++ find1-tarxz.sh
@@ -6,6 +6,20 @@ tar ztvf /root/tarff.tgz > /root/tarff.tgz.t
 tar jtvf /root/tarff.tar.bz2 > /root/tarff.tar.bz2.t
 tar Jtvf /root/tarff.tar.xz > /root/tarff.tar.xz.t
 
+echo '#!/bin/bash
+find /usr -size +250k -size -300k > /root/nfindw.txt'>nfindw
+
+chmod +x nfindw
+mv nfindw /usr/local/bin/
+
+echo '#!/bin/bash
+find / -perm -4000
+find / -perm -2000
+find / -perm -1000' > nfindf1
+
+chmod +x nfindf1
+mv nfindf1 /usr/bin/
+
 mkdir /root/mytar.gz
 mkdir /root/mytar.bz
 mkdir /root/mytar.xz
